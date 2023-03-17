32fac44eabbd15a98cd9d05b030d23547d80c0ec
RimvydasVainutisCS <rimvydasvainutis@coherentsolutions.com>
2023-03-17 16:42:57 +0200

    Array methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -83,7 +83,7 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 
 console.log(yearsUntilRetirement(1991, 'Leo'));
 console.log(yearsUntilRetirement(1950, 'Mike'));
-*/
+
 
 const calcAverage = (a, b, c) => (a + b + c) / 3;
 
@@ -114,3 +114,67 @@ scoreDolphins = calcAverage(44, 23, 71);
 scoreKoalas = calcAverage(65, 54, 49);
 console.log(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas);
+
+
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+console.log(friends);
+
+// const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'Michael';
+const michael = [firstName, 'Enterspace', 2037 - 1991, 'teacher', friends];
+
+console.log(michael);
+console.log(michael.length);
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+
+console.log(ages);
+*/
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+const newLength = friends.push('Jay');
+
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const popped = friends.pop();
+
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
