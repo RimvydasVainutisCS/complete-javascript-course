4b1df3ee4ad23b4939795ac5018237bf1fa19bd8
RimvydasVainutisCS <rimvydasvainutis@coherentsolutions.com>
2023-03-29 15:54:43 +0300

    loops done

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -312,7 +312,6 @@ if(mark.bmi > john.bmi) {
 for(let rep = 1; rep <= 10; rep++) {
     console.log(`Lifting weights repetition ${rep}.`);
 }
-*/
 
 const jonasArray = [
     'Jonas',
@@ -359,3 +358,75 @@ for(let i = 0; i < jonasArray.length; i++) {
     
     console.log(jonasArray[i], typeof jonasArray[i]);
 }
+
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+for(let i = jonasArray.length - 1; i >= 0; i--) {
+    console.log(i, jonasArray[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`----- Starting exercise ${exercise} -----`);
+
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}.`);
+    }
+}
+
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep}.`)
+// }
+
+let rep = 1;
+
+while (rep <= 10) {
+    //console.log(`WHILE: Lifting weights repetition ${rep}.`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}.`);
+    dice = Math.trunc(Math.random() * 6 + 1);
+    if (dice === 6) console.log('Loop is about to end...');
+}
+*/
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++){
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+const calcAverage = function(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        //sum = sum + arr[i];
+        sum += arr[i];
+    }
+    //console.log(sum);
+    return sum / arr.length;
+}
+
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
